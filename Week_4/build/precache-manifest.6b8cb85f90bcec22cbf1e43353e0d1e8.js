8febf4fbf178b9179c753dfb0647b2373fba8542
LennonX0904 <LennonX0904@gmail.com>
2019-02-03 23:04:53 +0800

    week4

---
+++ Week_4/build/precache-manifest.6b8cb85f90bcec22cbf1e43353e0d1e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bee36bb775304128ae2e20fcb30e4772",
+    "url": "/remote-assignments/static/media/tech_cover.bee36bb7.jpg"
+  },
+  {
+    "revision": "ad48c0c5dcdd9e739ae5",
+    "url": "/remote-assignments/static/js/runtime~main.ad48c0c5.js"
+  },
+  {
+    "revision": "a55b4866c8d131f361a9",
+    "url": "/remote-assignments/static/js/main.a55b4866.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/remote-assignments/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "a55b4866c8d131f361a9",
+    "url": "/remote-assignments/static/css/main.109d4b7b.chunk.css"
+  },
+  {
+    "revision": "521505a208b8493819e1d63e4b8eddcc",
+    "url": "/remote-assignments/index.html"
+  }
+];
